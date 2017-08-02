84dd414b24223bdbb130dd7f65a3e084f9befc2b
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-02 15:40:56 -0300

    Validacao dos campos do formulario / refatoracao da exibicao de altura e peso invalidos

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -2,7 +2,6 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
 var pacientes = document.querySelectorAll(".paciente");
-
 for(var i = 0; i < pacientes.length; i++){
 
 	var paciente = pacientes[i];
@@ -13,27 +12,27 @@ for(var i = 0; i < pacientes.length; i++){
 	var tdAltura = paciente.querySelector(".info-altura");
 	var altura = tdAltura.textContent;
 
-	var pesoEhValido = true;
-	var alturaEhValido = true;
+	var pesoEhValido = validaPeso(peso);
+	var alturaEhValido = validaAltura(altura);
 
 	var tdIMC = paciente.querySelector(".info-imc");
 
-	if(peso <= 0 || peso >= 500){
+	if(!pesoEhValido){
 		console.log("Peso inválido");
 		tdIMC.textContent = "Peso Inválido";
 		pesoEhValido = false;
+		paciente.classList.add("paciente-invalido");
 	}
 
-	if(altura <= 0 || altura >= 3.0){
+	if(!alturaEhValido){
 		console.log("Altura inválido");
 		tdIMC.textContent = "Altura Inválido";
 		alturaEhValido = false;
+		paciente.classList.add("paciente-invalido");
 	}
 
 	if(alturaEhValido && pesoEhValido){
 		tdIMC.textContent = calculaImc(peso, altura);
-	}else{
-		paciente.classList.add("paciente-invalido");
 	}
 
 }
@@ -45,4 +44,19 @@ function calculaImc(peso, altura) {
     return imc.toFixed(2);
 }
 
+function validaPeso(peso){
+	if(peso >= 0 && peso <= 1000){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function validaAltura(altura){
+	if(altura >= 0 && altura <= 3.0){
+		return true;
+	}else{
+		return false;
+	}
+}
 
--- js/form.js
+++ js/form.js
@@ -9,11 +9,24 @@ butaoAdicionar.addEventListener("click", function(event){
 
 	var pacienteTr = montaTr(paciente);
 
+	var erros = validaPaciente(paciente);
+
+	// verifica paciente invalido
+	if(erros.length > 0){
+		exibeMensagensDeErro(erros);
+		return; // sai da funcao
+	}
+
+	// adiciona paciente na tabela
 	var tabela = document.querySelector("#tabela-pacientes");
 
 	tabela.appendChild(pacienteTr);
 
 	form.reset();
+
+	var mensagensErro = document.querySelector("#mensagens-erro");
+
+	mensagensErro.innerHTML = "";
 
 });
 
@@ -50,4 +63,35 @@ function montaTd(dado, classe){
 	td.classList.add(classe);
 
 	return td;
+}
+
+function validaPaciente(paciente){
+
+	var erros = [];
+
+	if(paciente.nome.length == 0) erros.push("O nome não pode ser em branco");
+
+	if(paciente.gordura.length == 0) erros.push("A gordura não pode ser em branco");
+
+	if(paciente.peso.length == 0) erros.push("O peso não pode ser em branco");
+
+	if(paciente.altura.length == 0) erros.push("A altura não pode ser em branco");
+
+	if(!validaPeso(paciente.peso)) erros.push("Peso é Invalido");
+
+	if(!validaAltura(paciente.altura)) erros.push("Altura é Invalida");
+
+	return erros;
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+
+    ul.innerHTML = ""; // apaga as li da ul
+
+    erros.forEach(function(erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
 }
