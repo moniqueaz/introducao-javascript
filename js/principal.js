43c0167a4c6dbe3bf7549baf3ccaefbac63f5fb4
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-02 09:47:02 -0300

    Calculo de IMC de um paciente

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,24 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista";
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / (altura*2);
+
+var tdIMC = paciente.querySelector(".info-imc");
+
+tdIMC.textContent = imc;
+
+
+
+
+
+console.log(paciente);
+console.log(peso);
+console.log(altura);
