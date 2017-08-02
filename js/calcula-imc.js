790706b2077d6b9adb1ddc1343db58df700f634c
Monique Azevedo <moniqueaz.si@gmail.com>
2017-08-02 12:59:16 -0300

    Separando js calculo imc do js do form e incluindo arquivos na index

---
+++ js/calcula-imc.js
@@ -0,0 +1,42 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Aparecida Nutricionista";
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(var i = 0; i < pacientes.length; i++){
+
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente.querySelector(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector(".info-altura");
+	var altura = tdAltura.textContent;
+
+	var pesoEhValido = true;
+	var alturaEhValido = true;
+
+	var tdIMC = paciente.querySelector(".info-imc");
+
+	if(peso <= 0 || peso >= 500){
+		console.log("Peso inválido");
+		tdIMC.textContent = "Peso Inválido";
+		pesoEhValido = false;
+	}
+
+	if(altura <= 0 || altura >= 3.0){
+		console.log("Altura inválido");
+		tdIMC.textContent = "Altura Inválido";
+		alturaEhValido = false;
+	}
+
+	if(alturaEhValido && pesoEhValido){
+		var imc = peso / (altura*2);
+		tdIMC.textContent = imc.toFixed(2);
+	}else{
+		paciente.classList.add("paciente-invalido");
+	}
+
+}
+
+
